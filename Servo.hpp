887f410fa3251bba0dd2ba7a6a0b6afd245010c8
sho132767 <e2536@s.akashi.ac.jp>
2025-11-22 10:53:36 +0900

    localoca
    Commented out the old angle setting method and added a new implementation.

--- Servo.hpp
+++ Servo.hpp
@@ -13,23 +13,22 @@ public:
     
     Servo(PinName pin, int min = 500, int max = 2500) 
         : servo(pin), min(min), max(max), now_angle(0) {
-        servo.period_ms(20); 
-        set_angle(0);         
+        servo.period_ms(20);            
     }
 
    
-    void set_angle(int d) {
-        if (d < 0) d = 0;
-        if (d > 180) d = 180;
-        int target_angle = min + (max - min) * d / 180;
-        int step = (d > now_angle) ? 100 : -100;
-     for (int p = now_angle; (step > 0 ? p <= target_angle : p >= target_angle);  p += step) {
-        servo.pulsewidth_us(p);
-        ThisThread::sleep_for(10);
-    }
-    now_angle = d;
-    }
-   void fast_angle(int s) {
+    // void neta_angle(int d) {
+    //     if (d < 0) d = 0;
+    //     if (d > 180) d = 180;
+    //     int target_angle = min + (max - min) * d / 180;
+    //     int step = (d > now_angle) ? 100 : -100;
+    //  for (int p = now_angle; (step > 0 ? p <= target_angle : p >= target_angle);  p += step) {
+    //     servo.pulsewidth_us(p);
+    //     ThisThread::sleep_for(10);
+    // }
+    // now_angle = d;
+    // }
+   void set_angle(int s) {
         if (s < 0) s = 0;
         if (s > 180) s = 180;
         now_angle = s;
